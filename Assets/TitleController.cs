1d741dcf1abef7bf2936e274d5dde7a752535a63
DancingEngie <karmialon@gmail.com>
2021-06-13 17:58:53 +0300

    Created title screen and added it to build settings Configured WebGL build settings Re-added screen shake on ramming Some audio rebalances Made some UI more responsive to other screen resolutions (sorry! I had to change samplescene!)

---
+++ Assets/TitleController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+                Application.Quit();
+
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+                SceneManager.LoadScene(nextSceneName);
+    }
+}
