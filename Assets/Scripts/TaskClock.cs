b130151a612b03cf1e3204277b468ad6c7e2e76c
Omer Zadok <OZadok@gmail.com>
2021-06-13 19:50:06 +0300

    Task Clock fix

--- Assets/Scripts/TaskClock.cs
+++ Assets/Scripts/TaskClock.cs
@@ -13,12 +13,31 @@ public class TaskClock : MonoBehaviour
     {
         StateComponent.OnStartFix += OnStartFix;
         StateComponent.OnStopFix += OnStopFix;
+        StateComponent.OnStateEnter.AddListener(OnStateEnter);
+        StateComponent.OnStateExit.AddListener(OnStateExit);
         clockImage.enabled = false;
+    }
+
+    private void OnStateEnter(State state)
+    {
+        if (state == State.Criminal || state == State.Damaged || state == State.OnFire)
+        {
+            clockImage.enabled = true;
+        }
+    }
+    
+    private void OnStateExit(State state)
+    {
+        if (state == State.Criminal || state == State.Damaged || state == State.OnFire)
+        {
+            clockImage.enabled = false;
+        }
     }
 
     private void OnStopFix()
     {
-        clockImage.enabled = false;
+        clockImage.fillAmount = 1;
+        StopAllCoroutines();
     }
 
     private void OnStartFix(float timeToFix)
@@ -28,7 +47,7 @@ public class TaskClock : MonoBehaviour
 
     private IEnumerator FixEnumerator(float timeToFix)
     {
-        clockImage.enabled = true;
+        // clockImage.enabled = true;
         var startTime = Time.time;
         float fillAmount = 1 - (Time.time - startTime) / timeToFix;
         while (fillAmount > 0)
