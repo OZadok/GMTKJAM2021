ee2205f87e0ee1ed0415140564e12673bb0101c3
DancingEngie <karmialon@gmail.com>
2021-06-13 15:29:28 +0300

    Flair textbox behavior Text changes and optimizations

--- Assets/Scripts/FlairTextController.cs
+++ Assets/Scripts/FlairTextController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Text;
-using UnityEditor;
 
 public enum Conditions
 {
+    NONE,
     FIRE_BUILDING,
     FIRE_PEEP,
     FIRE_CRIMEDEN,
@@ -27,8 +27,13 @@ public class FlairTextController : MonoBehaviour
 {
     [SerializeField] private List<TextConditionals> textList = new List<TextConditionals>();
     [SerializeField] private GameObject textBoxPrefab;
+    [Tooltip("The chance, in percentage, that a message box will pop up every time a given state changes. Also note there can only be one text box at a time.")]
+    [SerializeField, Range(0,100)] private float messageChance = 10f;
+    [SerializeField] private Sound popSound;
 
     public static FlairTextController instance;
+
+    private Textbox currentTextbox;
 
     void Awake()
     {
@@ -45,9 +50,39 @@ public class FlairTextController : MonoBehaviour
     
     private void Start()
     {
-        //var path = Application.dataPath + "/Writing/" + textList[0].fileName + ".txt";
-        var path = AssetDatabase.GetAssetPath(textList[0].textAsset);
-        string[] lines = File.ReadAllLines(path);
-        //print(lines[Random.Range(0, lines.Length)]);
     }
+
+    public void CreateTextbox(Conditions condition)
+    {
+        if(currentTextbox != null)
+        {
+            print("Will not create textbox; Textbox is already filled");
+            return;
+        }
+
+        if (Random.Range(0,100) > messageChance)
+        {
+            print("Will not create textbox; random chance failed");
+            return;
+        }
+
+        if (condition == Conditions.NONE) return;
+
+        // Get variables
+        currentTextbox = Instantiate(textBoxPrefab, transform).GetComponent<Textbox>();
+        TextAsset conditionAsset = textList.Find(item => item.condition == condition).textAsset;
+
+        // Get text and spawn it
+        currentTextbox.Init(GetRandomLineFromTextasset(conditionAsset));
+
+        // Play SFX
+        AudioManager.instance.Play(popSound, false);
+    }
+
+    private string GetRandomLineFromTextasset(TextAsset ta)
+    {
+        var arrayString = ta.text.Split('\n');
+        return arrayString[Random.Range(0, arrayString.Length)];
+    }
+
 }
